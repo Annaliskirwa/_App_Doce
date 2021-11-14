15494563019abc0437d8da8e205de25304202365
Annaliskirwa <annaliskirwa@gmail.com>
2021-11-14 15:21:49 +0300

    Components, interpolation, property and event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item/media-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItemComponent
   ],
   imports: [
     BrowserModule,
