2e7e63024f5ce90ac1ae84d878320e1283012dd4
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-07 00:18:49 +0300

    Error handling invalid name and year

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -36,7 +36,10 @@ export class MediaItemFormComponent implements OnInit {
     if (year >= minYear && year <= maxYear) {
       return null;
     } else {
-      return { year: true };
+      return { year: {
+        min: minYear,
+        max: maxYear
+      } };
     }
   }
 
