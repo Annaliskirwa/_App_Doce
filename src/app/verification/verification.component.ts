46edfd76836e728db408cd5437ad19c35c829bda
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-10 08:39:29 +0300

    Added the code input template and script functions

--- src/app/verification/verification.component.ts
+++ src/app/verification/verification.component.ts
@@ -13,4 +13,12 @@ export class VerificationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  // this called every time when user changed the code
+  onCodeChanged(code: string) {
+  }
+
+  // this called only if user entered full code
+  onCodeCompleted(code: string) {
+  }
 }
