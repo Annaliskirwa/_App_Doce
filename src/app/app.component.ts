9d06c890b7d0d2def76d7725ed845ce01dbc329f
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-11 17:41:50 +0300

    Emulated encapsulation mode for styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit, ViewEncapsulation } from '@angular/core';
 import { MediaItemService, MediaItem} from './media-item.service';
 
 @Component({
   selector: 'mw-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 
 
